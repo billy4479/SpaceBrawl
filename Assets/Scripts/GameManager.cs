5675e8acbb45dd1235cdaeab3735b93dc985b8f8
billy4479 <giachi.ellero@gmail.com>
2020-01-16 19:27:30 +0100

    Full working enemy system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -63,63 +63,6 @@ public class GameManager : MonoBehaviour
         this.SpawnNewEnemies(this.Level);
     }
 
-    private void CreateBorders()
-    {
-        //Generate our empty objects
-        topCollider = new GameObject().transform;
-        bottomCollider = new GameObject().transform;
-        rightCollider = new GameObject().transform;
-        leftCollider = new GameObject().transform;
-
-        //Name our objects 
-        topCollider.name = "TopCollider";
-        bottomCollider.name = "BottomCollider";
-        rightCollider.name = "RightCollider";
-        leftCollider.name = "LeftCollider";
-
-        //Add tag
-        topCollider.tag = "Border";
-        bottomCollider.tag = "Border";
-        rightCollider.tag = "Border";
-        leftCollider.tag = "Border";
-
-        //Add the colliders
-        topCollider.gameObject.AddComponent<BoxCollider2D>();
-        bottomCollider.gameObject.AddComponent<BoxCollider2D>();
-        rightCollider.gameObject.AddComponent<BoxCollider2D>();
-        leftCollider.gameObject.AddComponent<BoxCollider2D>();
-
-        //Add materials to Colliders
-        PhysicsMaterial2D mat = new PhysicsMaterial2D();
-        mat.bounciness = this.bounciness;
-        mat.friction = this.friction;
-        topCollider.GetComponent<BoxCollider2D>().sharedMaterial = mat;
-        bottomCollider.GetComponent<BoxCollider2D>().sharedMaterial = mat;
-        rightCollider.GetComponent<BoxCollider2D>().sharedMaterial = mat;
-        leftCollider.GetComponent<BoxCollider2D>().sharedMaterial = mat;
-
-        //Make them the child of whatever object this script is on, preferably on the Camera so the objects move with the camera without extra scripting
-        topCollider.parent = Camera.main.transform;
-        bottomCollider.parent = Camera.main.transform;
-        rightCollider.parent = Camera.main.transform;
-        leftCollider.parent = Camera.main.transform;
-
-        //Generate world space point information for position and scale calculations
-        cameraPos = Camera.main.transform.position;
-        screenSize.x = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0))) * 0.5f;
-        screenSize.y = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height))) * 0.5f;
-
-        //Change our scale and positions to match the edges of the screen...   
-        rightCollider.localScale = new Vector3(colDepth, screenSize.y * 2, colDepth);
-        rightCollider.position = new Vector3(cameraPos.x + screenSize.x + (rightCollider.localScale.x * 0.5f), cameraPos.y, zPosition);
-        leftCollider.localScale = new Vector3(colDepth, screenSize.y * 2, colDepth);
-        leftCollider.position = new Vector3(cameraPos.x - screenSize.x - (leftCollider.localScale.x * 0.5f), cameraPos.y, zPosition);
-        topCollider.localScale = new Vector3(screenSize.x * 2, colDepth, colDepth);
-        topCollider.position = new Vector3(cameraPos.x, cameraPos.y + screenSize.y + (topCollider.localScale.y * 0.5f), zPosition);
-        bottomCollider.localScale = new Vector3(screenSize.x * 2, colDepth, colDepth);
-        bottomCollider.position = new Vector3(cameraPos.x, cameraPos.y - screenSize.y - (bottomCollider.localScale.y * 0.5f), zPosition);
-
-    }
 
     private IEnumerator PlayerWon()
     {
@@ -260,5 +203,62 @@ public class GameManager : MonoBehaviour
 
         if (this.EnemyNumber == 0)
             StartCoroutine(this.PlayerWon());
+    }
+    private void CreateBorders()
+    {
+        //Generate our empty objects
+        topCollider = new GameObject().transform;
+        bottomCollider = new GameObject().transform;
+        rightCollider = new GameObject().transform;
+        leftCollider = new GameObject().transform;
+
+        //Name our objects 
+        topCollider.name = "TopCollider";
+        bottomCollider.name = "BottomCollider";
+        rightCollider.name = "RightCollider";
+        leftCollider.name = "LeftCollider";
+
+        //Add tag
+        topCollider.tag = "Border";
+        bottomCollider.tag = "Border";
+        rightCollider.tag = "Border";
+        leftCollider.tag = "Border";
+
+        //Add the colliders
+        topCollider.gameObject.AddComponent<BoxCollider2D>();
+        bottomCollider.gameObject.AddComponent<BoxCollider2D>();
+        rightCollider.gameObject.AddComponent<BoxCollider2D>();
+        leftCollider.gameObject.AddComponent<BoxCollider2D>();
+
+        //Add materials to Colliders
+        PhysicsMaterial2D mat = new PhysicsMaterial2D();
+        mat.bounciness = this.bounciness;
+        mat.friction = this.friction;
+        topCollider.GetComponent<BoxCollider2D>().sharedMaterial = mat;
+        bottomCollider.GetComponent<BoxCollider2D>().sharedMaterial = mat;
+        rightCollider.GetComponent<BoxCollider2D>().sharedMaterial = mat;
+        leftCollider.GetComponent<BoxCollider2D>().sharedMaterial = mat;
+
+        //Make them the child of whatever object this script is on, preferably on the Camera so the objects move with the camera without extra scripting
+        topCollider.parent = Camera.main.transform;
+        bottomCollider.parent = Camera.main.transform;
+        rightCollider.parent = Camera.main.transform;
+        leftCollider.parent = Camera.main.transform;
+
+        //Generate world space point information for position and scale calculations
+        cameraPos = Camera.main.transform.position;
+        screenSize.x = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0))) * 0.5f;
+        screenSize.y = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height))) * 0.5f;
+
+        //Change our scale and positions to match the edges of the screen...   
+        rightCollider.localScale = new Vector3(colDepth, screenSize.y * 2, colDepth);
+        rightCollider.position = new Vector3(cameraPos.x + screenSize.x + (rightCollider.localScale.x * 0.5f), cameraPos.y, zPosition);
+        leftCollider.localScale = new Vector3(colDepth, screenSize.y * 2, colDepth);
+        leftCollider.position = new Vector3(cameraPos.x - screenSize.x - (leftCollider.localScale.x * 0.5f), cameraPos.y, zPosition);
+        topCollider.localScale = new Vector3(screenSize.x * 2, colDepth, colDepth);
+        topCollider.position = new Vector3(cameraPos.x, cameraPos.y + screenSize.y + (topCollider.localScale.y * 0.5f), zPosition);
+        bottomCollider.localScale = new Vector3(screenSize.x * 2, colDepth, colDepth);
+        bottomCollider.position = new Vector3(cameraPos.x, cameraPos.y - screenSize.y - (bottomCollider.localScale.y * 0.5f), zPosition);
+
     }
 }
