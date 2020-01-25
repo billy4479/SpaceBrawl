7f233532e54574ef518b6e339a37fa4d52e2b906
billy4479 <giachi.ellero@gmail.com>
2020-01-25 12:41:55 +0100

    Mappa grande funzionante, sfondo da sistemare

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameManager gm;
+    public GameObject background;
+
+    private Vector3 player;
+    private Vector3 oldPlayer;
+    public List<GameObject> anchors;
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, -10);
+        background.transform.DetachChildren();
+    }
+
+    void FixedUpdate()
+    {
+        try
+        {
+            player = gm.CurrentPlayer.transform.position;
+        }
+        catch
+        {
+            player = oldPlayer;
+        }
+        Vector3 distFromOne = player - anchors[0].transform.position;
+        Vector3 distFromTwo = player - anchors[1].transform.position;
+
+        bool followOnlyY = false;
+        bool followOnlyX = false;
+
+        if (distFromOne.x > -gm.screenSize.x)
+        {
+            followOnlyY = true;
+        }
+        if (distFromOne.y > -gm.screenSize.y)
+        {
+            followOnlyX = true;
+        }
+        if (distFromTwo.x < gm.screenSize.x)
+        {
+            followOnlyY = true;
+        }
+        if (distFromTwo.y < gm.screenSize.y)
+        {
+            followOnlyX = true;
+        }
+
+        if (followOnlyX && followOnlyY)
+        { }
+        else if (followOnlyX)
+        {
+            transform.position = new Vector3(player.x, transform.position.y, -10f);
+        }
+        else if (followOnlyY)
+        {
+            transform.position = new Vector3(transform.position.x, player.y, -10f);
+        }
+        else
+        {
+            transform.position = new Vector3(player.x, player.y, -10f);
+        }
+        oldPlayer = player;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, gm.screenSize.x);
+        Gizmos.DrawWireSphere(transform.position, gm.screenSize.y);
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -36,19 +36,12 @@ public class GameManager : MonoBehaviour
     public float bounciness;
     [Range(0f, 1f)]
     public float friction;
-    private GameObject CurrentPlayer;
+    public GameObject CurrentPlayer;
     private AudioManager audioManager;
 
     private enum sides { UP, DOWN, RIGHT, LEFT };
 
-    //Bordo
-    private float colDepth = 1f;
-    private float zPosition = 0f;
-    private Vector2 screenSize;
-    private Transform topCollider;
-    private Transform bottomCollider;
-    private Transform leftCollider;
-    private Transform rightCollider;
+    public Vector2 screenSize;
     private Vector3 cameraPos;
 
 
@@ -59,16 +52,22 @@ public class GameManager : MonoBehaviour
         this.Level = 1;
         this.EnemyNumber = 0;
         this.SuspendInput = false;
+        cameraPos = Camera.main.transform.position;
 
         audioManager = AudioManager.instance;
 
-        CreateBorders();
+    }
 
+
+    private void Start()
+    {
         CurrentPlayer = Instantiate(this.PlayerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+
+        screenSize.x = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0))) * 0.5f;
+        screenSize.y = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height))) * 0.5f;
 
         this.SpawnNewEnemies(this.Level);
     }
-
 
     private IEnumerator PlayerWon()
     {
@@ -174,8 +173,6 @@ public class GameManager : MonoBehaviour
 
     private Vector3 RandomSpawnPosOnBorders()
     {
-        screenSize.x = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0))) * 0.5f;
-        screenSize.y = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height))) * 0.5f;
         Vector3 result = new Vector3(0, 0, 0);
         sides side = (sides)Random.Range(0, 4);
 
@@ -201,70 +198,16 @@ public class GameManager : MonoBehaviour
         return result;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         this.LifesLabel.text = "Lifes: " + this.Lifes;
         this.ScoreLabel.text = "Score: " + this.Score;
         this.LevelLabel.text = "Level: " + this.Level;
-
-        if (this.EnemyNumber == 0)
-            StartCoroutine(this.PlayerWon());
-    }
-    private void CreateBorders()
-    {
-        //Generate our empty objects
-        topCollider = new GameObject().transform;
-        bottomCollider = new GameObject().transform;
-        rightCollider = new GameObject().transform;
-        leftCollider = new GameObject().transform;
-
-        //Name our objects 
-        topCollider.name = "TopCollider";
-        bottomCollider.name = "BottomCollider";
-        rightCollider.name = "RightCollider";
-        leftCollider.name = "LeftCollider";
-
-        //Add tag
-        topCollider.tag = "Border";
-        bottomCollider.tag = "Border";
-        rightCollider.tag = "Border";
-        leftCollider.tag = "Border";
-
-        //Add the colliders
-        topCollider.gameObject.AddComponent<BoxCollider2D>();
-        bottomCollider.gameObject.AddComponent<BoxCollider2D>();
-        rightCollider.gameObject.AddComponent<BoxCollider2D>();
-        leftCollider.gameObject.AddComponent<BoxCollider2D>();
-
-        //Add materials to Colliders
-        PhysicsMaterial2D mat = new PhysicsMaterial2D();
-        mat.bounciness = this.bounciness;
-        mat.friction = this.friction;
-        topCollider.GetComponent<BoxCollider2D>().sharedMaterial = mat;
-        bottomCollider.GetComponent<BoxCollider2D>().sharedMaterial = mat;
-        rightCollider.GetComponent<BoxCollider2D>().sharedMaterial = mat;
-        leftCollider.GetComponent<BoxCollider2D>().sharedMaterial = mat;
-
-        //Make them the child of whatever object this script is on, preferably on the Camera so the objects move with the camera without extra scripting
-        topCollider.parent = Camera.main.transform;
-        bottomCollider.parent = Camera.main.transform;
-        rightCollider.parent = Camera.main.transform;
-        leftCollider.parent = Camera.main.transform;
-
-        //Generate world space point information for position and scale calculations
-        cameraPos = Camera.main.transform.position;
         screenSize.x = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0))) * 0.5f;
         screenSize.y = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height))) * 0.5f;
 
-        //Change our scale and positions to match the edges of the screen...   
-        rightCollider.localScale = new Vector3(colDepth, screenSize.y * 2, colDepth);
-        rightCollider.position = new Vector3(cameraPos.x + screenSize.x + (rightCollider.localScale.x * 0.5f), cameraPos.y, zPosition);
-        leftCollider.localScale = new Vector3(colDepth, screenSize.y * 2, colDepth);
-        leftCollider.position = new Vector3(cameraPos.x - screenSize.x - (leftCollider.localScale.x * 0.5f), cameraPos.y, zPosition);
-        topCollider.localScale = new Vector3(screenSize.x * 2, colDepth, colDepth);
-        topCollider.position = new Vector3(cameraPos.x, cameraPos.y + screenSize.y + (topCollider.localScale.y * 0.5f), zPosition);
-        bottomCollider.localScale = new Vector3(screenSize.x * 2, colDepth, colDepth);
-        bottomCollider.position = new Vector3(cameraPos.x, cameraPos.y - screenSize.y - (bottomCollider.localScale.y * 0.5f), zPosition);
 
+        if (this.EnemyNumber == 0)
+            StartCoroutine(this.PlayerWon());
     }
 }
